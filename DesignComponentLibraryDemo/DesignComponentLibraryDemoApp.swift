320e2388665e570182d077f7f6ef41243d7ba7d7
knowitAli <ali.haider@knowit.se>
2023-05-30 14:36:14 +0200

    Initial Commit

---
+++ DesignComponentLibraryDemo/DesignComponentLibraryDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignComponentLibraryDemoApp.swift
+//  DesignComponentLibraryDemo
+//
+//  Created by Ali on 2023-05-30.
+//
+
+import SwiftUI
+
+@main
+struct DesignComponentLibraryDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
